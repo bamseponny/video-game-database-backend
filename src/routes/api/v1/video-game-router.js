b13d9030af608c68a685fa219633651bb4e13c65
Fredrik Eriksson <ferth09@student.lnu.se>
2022-04-13 08:05:09 +0200

    Add the different routes for the game data base.

--- src/routes/api/v1/video-game-router.js
+++ src/routes/api/v1/video-game-router.js
@@ -5,10 +5,25 @@
  * @version 1.0.0
  */
 import express from 'express'
-/* import { VideoGameController } from '../../../controllers/api/video-game-controller.js' */
+import { VideoGameController } from '../../../controllers/api/video-game-controller.js'
 
 export const router = express.Router()
 
-/* const controller = new VideoGameController() */
+const controller = new VideoGameController()
 
-router.get('/games', (req, res) => res.json({ message: 'Yay! Just yay!' }))
+// Map HTTP verbs and route paths to controller actions.
+
+// List all video games of a certain format.
+router.get('/games', (req, res, next) => controller.listAllGames(req, res, next))
+
+// Add a video game to the database.
+router.post('/games', (req, res, next) => controller.addGame(req, res, next))
+
+// Show single game.
+router.get('/games/:id', (req, res, next) => controller.findOneGame(req, res, next))
+
+// Edit game.
+router.put('/games/:id', (req, res, next) => controller.updateGame(req, res, next))
+
+// Delete game.
+router.get('/games/:id', (req, res, next) => controller.deleteGame(req, res, next))
